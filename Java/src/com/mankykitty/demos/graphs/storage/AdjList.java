3caf0f35e5239ec3d033848b8cd554fe95cfa0d3
Sean Chalmers <sclhiannan@gmail.com>
2014-02-28 13:49:04 +1000

    Fix typo

--- Java/src/com/mankykitty/demos/graphs/storage/AdjList.java
+++ Java/src/com/mankykitty/demos/graphs/storage/AdjList.java
@@ -1,27 +1,27 @@
 
 
-public class Graph<T> {
-  HashMap<T, Integer> indicies;
+class AdjList<T> {
+  HashMap<T, Integer> indices;
   ArrayList<Node> g;
 
-  public Graph() {
-    this.indicies = new HashMap<T, Integer>();
+  public AdjList() {
+    this.indices = new HashMap<T, Integer>();
     this.g = new ArrayList<ArrayList<Integer>>();
-  } 
+  }
 
   public boolean has(T v) {
-    return this.indicies.containsKey(v);
+    return this.indices.containsKey(v);
   }
 
   public boolean addEdge(T start, T end) {
-    if (this.has(start) && this.has(end)) {
-      
-      int startI = this.indicies.get(start);
-      int endI = this.indicies.get(end);
+    if (!this.hasEdge(start, end)) {
+
+      int startI = this.indices.get(start);
+      int endI = this.indices.get(end);
 
       this.setEdge(start, endI);
       this.setEdge(end, startI);
-      
+
     }
     return false;
   }
@@ -34,43 +34,38 @@ public class Graph<T> {
   private void setEdge(T start, int endI) {
     this.getVertex(start).edges.add(endI);
   }
-  
+
   private boolean vertexHasEdge(T start, T end) {
-    this.getVertex(start).edges.contains(this.indicies.get(end));
+    return this.getVertex(start).edges.contains(this.indices.get(end));
   }
-  
+
   public boolean addVertex(T v) {
     Node newVertex;
-    int indexOf;
-    
+
     if (!this.has(v)) {
 
       // Create our new node and add it to the graph
       newVertex = new Node(v);
       this.g.add(newVertex);
       // Store the index of this node for quick retrieval later
-      this.indicies.put(v, this.g.indexOf(newVertex));
-      
+      this.indices.put(v, this.g.indexOf(newVertex));
+
       return true;
     }
     return false;
   }
 
-  /***
-   * Private helper functions
-   */
-  
   private Node getVertex(T value) {
-    return this.g.get(this.indicies.get(value));
+    return this.g.get(this.indices.get(value));
   }
 }
 
-class Node<T> implements {
+class Node<T> {
   T value;
   ArrayList<Integer> edges;
 
-  public Node() {
-    this.value = -1;
+  public Node(T v) {
+    this.value = v;
     this.edges = new ArrayList<Integer>();
   }
 }
